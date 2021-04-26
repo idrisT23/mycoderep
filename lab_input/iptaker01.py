7c0c9ccf18ae1356d1f875aa563c59150a8c0188
Idris Taher <idris.taher@gmail.com>
2021-04-26 18:32:46 +0000

    mixed lists

--- lab_input/iptaker01.py
+++ lab_input/iptaker01.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
-user_input = input("Please enter an IPv4 IP address:")
-print("You told me the IPv4 address is:" + user_input)
-
+user_name = input("Please enter your name:")
+week_day = input("Please enter the day of the week:")
+print ("Hello," , user_name , "! Happy" , week_day, "!")
+print ("Hello, " + user_name + "! Happy " + week_day + "!")
